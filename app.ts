60b2ad7b4e79575c37ea7b2ebdbadf60a05a75f2
Navaneeth <vnavaneeth15@gmail.com>
2020-09-18 17:57:07 +0530

    added travis ci

--- app.ts
+++ app.ts
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-	res.send('welcome to our world');
+	res.send('from the CI pipeline');
 });
 
 app.listen(PORT, () => {
